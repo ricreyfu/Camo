6a3e7628f153e7130f3160e4b7a760ed5792db7d
ricreyfu <ricreyfu@gmail.com>
2026-06-28 23:00:51 +0200

    Update room.js

--- api/room.js
+++ api/room.js
@@ -16,6 +16,7 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,POST,OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Cache-Control', 'no-store, max-age=0');
 
   if (req.method === 'OPTIONS') {
     return res.status(200).end();
